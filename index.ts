243e939d81cbdccef563a0586c8a349fe32b7f47
jimao <jimao@jimaodeMacBook-Pro.local>
2022-07-08 14:44:22 +0800

    buyall

--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
 import { binance } from 'ccxt';
 import secret from './.secret.json';
-
-console.log('你好，世界');
-console.log(secret.SECRET_KEY);
+import { BinanceSpot } from './executor/spot/binance_spot';
 
 async function main() {
+  console.log('加载客户端...');
   const client = new binance({
     apiKey: secret.API_KEY,
     secret: secret.SECRET_KEY,
     enableRateLimit: true,
   });
   await client.loadMarkets();
+  console.log('客户端加载完成');
+  const executor = new BinanceSpot('LINK/USDT', client, 3);
+  await executor.BuyAll();
 }
 
 main();
