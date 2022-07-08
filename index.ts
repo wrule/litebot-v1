71542179fb0316db8e8c525dedc4eebc5c16d7be
jimao <jimao@jimaodeMacBook-Pro.local>
2022-07-08 14:37:45 +0800

    创建客户端

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
+import { binance } from 'ccxt';
+import secret from './.secret.json';
 
 console.log('你好，世界');
+console.log(secret.SECRET_KEY);
+
+async function main() {
+  const client = new binance({
+    apiKey: secret.API_KEY,
+    secret: secret.SECRET_KEY,
+    enableRateLimit: true,
+  });
+  await client.loadMarkets();
+}
+
+main();
