19d1f1da360251fb1f2f68541a29d2e255287b96
jimao <jimao@jimaodeMacBook-Pro.local>
2022-07-11 10:04:38 +0800

    机器人方法包装

--- robot/spot/index.ts
+++ robot/spot/index.ts
@@ -7,5 +7,35 @@ abstract class SpotRobot {
     protected executor: ISpotExecutor,
   ) { }
 
+  public Buy(
+    in_asset: number,
+    price?: number,
+    time?: number,
+  ) {
+    return this.executor.Buy(in_asset, price, time);
+  }
+
+  public BuyAll(
+    price?: number,
+    time?: number,
+  ) {
+    return this.executor.BuyAll(price, time);
+  }
+
+  public Sell(
+    in_asset: number,
+    price?: number,
+    time?: number,
+  ) {
+    return this.executor.Sell(in_asset, price, time);
+  }
+
+  public SellAll(
+    price?: number,
+    time?: number,
+  ) {
+    return this.executor.SellAll(price, time);
+  }
+
   public abstract CheckFastTest<T extends IOHLCV>(data: T): void;
 }
