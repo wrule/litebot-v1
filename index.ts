863ba28d9049cf296ea36f17c3de108b16f4eaea
jimao <jimao@jimaodeMacBook-Pro.local>
2022-07-11 14:10:04 +0800

    机器人运行

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import secret from './.secret.json';
 import { BinanceSpot } from './executor/spot/binance_spot';
 import { retryer } from './utils/retryer';
 import { write_list, read_list, append_list, delete_list } from './utils/json_list';
+import { TwoMaCross } from './robot/spot/two_ma_cross';
+import { ArrayToKLine } from './common/kline';
 
 async function main() {
   console.log('加载客户端...');
@@ -13,12 +15,18 @@ async function main() {
   });
   await client.loadMarkets();
   console.log('客户端加载完成');
-  const executor = new BinanceSpot('LINK/USDT', client, 3, 'tn_log.json');
-  try {
-    await executor.SellAll();
-  } catch (e) {
-    console.log(e);
-  }
+  const executor = new BinanceSpot('BTC/USDT', client, 3, 'tn_log.json');
+  const robot = new TwoMaCross(executor, 10, 30);
+  setInterval(async () => {
+    try {
+      const list = await client.fetchOHLCV('BTC/USDT', '1m', undefined, robot.KLineReadyLength);
+      const kline = ArrayToKLine(list);
+      console.log(new Date(), kline.length);
+      robot.CheckKLine(kline);
+    } catch (e) {
+      console.error(e);
+    }
+  }, 1000);
 }
 
 main();
