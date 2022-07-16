b058eb77d84c168e7e5f1fa2dd5ae9bc13c41a7e
jimao <jimao@jimaodeMBP.lan>
2022-07-16 18:57:40 +0800

    提交

--- executor/spot/index.ts
+++ executor/spot/index.ts
@@ -2,6 +2,26 @@ import { ReturnTransactionAS } from '../../common/transaction';
 
 export
 interface ISpotExecutor {
+  /**
+   * 资金名称
+   */
+  FundName: string;
+
+  /**
+   * 可用资金余额
+   */
+  FundBalance(): number | Promise<number>;
+
+  /**
+   * 资产名称
+   */
+  AssetName: string;
+
+  /**
+   * 可用资产余额
+   */
+  AssetBalance(): number | Promise<number>;
+
   Buy(
     in_asset: number,
     price?: number,
@@ -28,24 +48,4 @@ interface ISpotExecutor {
    * 重置测试机器人
    */
   Reset(): void;
-
-  /**
-   * 资金名称
-   */
-  FundName: string;
-
-  /**
-   * 可用资金余额
-   */
-  FundBalance(): number | Promise<number>;
-
-  /**
-   * 资产名称
-   */
-  AssetName: string;
-
-  /**
-   * 可用资产余额
-   */
-  AssetBalance(): number | Promise<number>;
 }
