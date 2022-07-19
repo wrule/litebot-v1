891564eb5ea144d47300d14a14854486d4f27359
jimao <jimao@jimaodeMacBook-Pro.local>
2022-07-19 21:28:05 +0800

    注释

--- executor/spot/index.ts
+++ executor/spot/index.ts
@@ -1,5 +1,9 @@
+// 2022年07月19日21:26:55
 import { ITransaction } from '@/common/transaction';
 
+/**
+ * 现货执行者接口
+ */
 export
 interface ISpotExecutor {
   /**
@@ -22,23 +26,49 @@ interface ISpotExecutor {
    */
   AssetBalance(): number | Promise<number>;
 
+  /**
+   * 购买资产
+   * @param in_asset 期望输入资产
+   * @param price 期望成交价
+   * @param time 交易时间（回测用）
+   * @returns 交易数据
+   */
   Buy(
     in_asset: number,
     price?: number,
     time?: number,
   ): ITransaction | Promise<ITransaction>;
 
+  /**
+   * 使用全部资金购买资产
+   * @param price 期望成交价
+   * @param time 交易时间（回测用）
+   * @returns 交易数据
+   */
   BuyAll(
     price?: number,
     time?: number,
   ): ITransaction | Promise<ITransaction>;
 
+  /**
+   * 出售资产
+   * @param in_asset 期望输入资产
+   * @param price 期望成交价
+   * @param time 交易时间（回测用）
+   * @returns 交易数据
+   */
   Sell(
     in_asset: number,
     price?: number,
     time?: number,
   ): ITransaction | Promise<ITransaction>;
 
+  /**
+   * 出售全部资产
+   * @param price 期望成交价
+   * @param time 交易时间（回测用）
+   * @returns 交易数据
+   */
   SellAll(
     price?: number,
     time?: number,
