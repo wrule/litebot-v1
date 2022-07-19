99d05b3a9d076a6bf372bb2519cb3769eaed3946
jimao <jimao@jimaodeMacBook-Pro.local>
2022-07-19 21:03:35 +0800

    feat: 提交代码

--- watcher/index.ts
+++ watcher/index.ts
@@ -1,16 +1,37 @@
 
+/**
+ * 监听回调函数
+ */
+export
+type WatcherCallBack<T> = (data: T) => void;
+
+/**
+ * 监听者抽象类
+ */
 export
 abstract class Watcher<T> {
-  private callback_map = new Map<(data: T) => void, void>();
+  private callback_map = new Map<WatcherCallBack<T>, void>();
 
-  public Subscribe(callback: (data: T) => void) {
+  /**
+   * 订阅更新
+   * @param callback 回调函数
+   */
+  public Subscribe(callback: WatcherCallBack<T>) {
     this.callback_map.set(callback);
   }
 
-  public Unsubscribe(callback: (data: T) => void) {
+  /**
+   * 取消订阅
+   * @param callback 回调函数
+   */
+  public Unsubscribe(callback: WatcherCallBack<T>) {
     this.callback_map.delete(callback);
   }
 
+  /**
+   * 更新数据方法
+   * @param data 数据
+   */
   protected update(data: T) {
     Array.from(this.callback_map.keys())
       .forEach((callback) => {
@@ -18,7 +39,13 @@ abstract class Watcher<T> {
       });
   }
 
+  /**
+   * 运行监听者
+   */
   public abstract Start(): void;
 
+  /**
+   * 关闭监听者
+   */
   public abstract Stop(): void;
 }
