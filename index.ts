184d318727ffb958744a57a4ffdb537738aa724d
jimao <jimao@jimaodeMacBook-Pro.local>
2022-07-22 00:32:04 +0800

    回测性能还是可以的

--- index.ts
+++ index.ts
@@ -4,12 +4,21 @@ import HistData from './data/ETH_USDT-2h.json';
 import { ArrayToKLine } from './common/kline';
 
 async function main() {
+  let testData: any[] = [];
+  console.log('开始');
   const executor = new TestSpot(100, 0.001, false, 'USDT', 'ETH');
-  const robot = new TwoMaCross({ fast_ma: 10, slow_ma: 21 }, executor);
-  const testData = robot.GenerateTestData(ArrayToKLine(HistData));
-  robot.BackTesting(testData);
-  const v = await robot.Executor.Valuation(3026.85);
-  console.log(v);
+  for (let fast = 2; fast < 200; ++fast) {
+    for (let slow = fast + 1; slow < 200; ++slow) {
+      const robot = new TwoMaCross({ fast_ma: fast, slow_ma: slow }, executor);
+      if (testData.length < 1) {
+        testData = robot.GenerateTestData(ArrayToKLine(HistData));
+      }
+      robot.BackTesting(testData);
+      const v = await robot.Executor.Valuation(3026.85);
+      // console.log(fast, v);
+    }
+  }
+  console.log('结束');
 }
 
 main();
