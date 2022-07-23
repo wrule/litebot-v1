ae74552f3a8a1f6d9a6d58f0e870e4b5f6b008fe
jimao <jimao@jimaodeMacBook-Pro.local>
2022-07-23 23:22:49 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -4,18 +4,19 @@ import HistData from './data/ETH_USDT-2h.json';
 import { ArrayToKLine } from './common/kline';
 
 async function main() {
-  let testData: any[] = [];
+  const realData = ArrayToKLine(HistData);
+  let maxValuation = -1;
   console.log('开始');
   const executor = new TestSpot(100, 0.001, false, 'USDT', 'ETH');
   for (let fast = 2; fast < 200; ++fast) {
     for (let slow = fast + 1; slow < 200; ++slow) {
       const robot = new TwoMaCross({ fast_ma: fast, slow_ma: slow }, executor);
-      if (testData.length < 1) {
-        testData = robot.GenerateTestData(ArrayToKLine(HistData));
+      robot.BackTesting(realData);
+      const valuation = await robot.Executor.Valuation(3026.85);
+      if (valuation > maxValuation) {
+        maxValuation = valuation;
+        console.log(fast, slow, valuation);
       }
-      robot.BackTesting(testData);
-      const v = await robot.Executor.Valuation(3026.85);
-      // console.log(fast, v);
     }
   }
   console.log('结束');
