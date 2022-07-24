c1b79e6c4b8e3fe5b95c3eb56a3f3557cc578cdb
jimao <jimao@jimaodeMBP.lan>
2022-07-24 14:50:09 +0800

    list

--- utils/list/json_list.ts
+++ utils/list/json_list.ts
@@ -1,15 +1,20 @@
 import { IList } from '.';
+import fs from 'fs';
 
 export
 class JSONList<T>
 implements IList<T> {
-  public constructor() { }
+  public constructor(
+    private readonly file: string,
+  ) { }
 
   public Append(data: T) {
-
+    fs.appendFileSync(this.file, JSON.stringify(data) + ',\n', 'utf-8');
   }
 
   public All() {
-    return [] as T[];
+    let json_text = fs.readFileSync(this.file, 'utf-8');
+    json_text = `[${json_text.substring(0, json_text.length - 2)}]`;
+    return JSON.parse(json_text) as T[];
   }
 }
