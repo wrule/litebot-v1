8c53ed39a093e63fdc06b63293745a363a737a58
jimao <jimao@jimaodeMBP.lan>
2022-07-24 17:26:04 +0800

    iprops

--- report/index.ts
+++ report/index.ts
@@ -3,6 +3,13 @@ import { ISnapshot } from '@/executor/spot';
 import { IList } from '@/utils/list';
 
 export
+interface IProps {
+  name?: string;
+  start_time?: number;
+  end_time?: number;
+}
+
+export
 class Report {
   public constructor(
     private readonly transactions: IList<ITransaction>,
