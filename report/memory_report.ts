4a852e500a6051825913ce584d136b7fbf829c9b
jimao <jimao@jimaodeMacBook-Pro.local>
2022-07-30 22:15:20 +0800

    内存报告

---
+++ report/memory_report.ts
@@ -0,0 +1,23 @@
+import { IOHLCV } from '@/common/kline';
+import { ITransaction } from '@/common/transaction';
+import { ISnapshot } from '@/executor/spot';
+import { MemoryList } from '@/utils/list/memory_list';
+import { IReportMeta, Report } from '.';
+
+export
+class MemoryReport<
+  Params,
+  RealData extends IOHLCV,
+  TestData extends IOHLCV,
+>
+extends Report<Params, RealData, TestData> {
+  public constructor() {
+    super({
+      meta_data: new MemoryList<IReportMeta<Params>>,
+      real_data: new MemoryList<RealData>(),
+      test_data: new MemoryList<TestData>(),
+      transactions: new MemoryList<ITransaction>(),
+      snapshots: new MemoryList<ISnapshot>(),
+    });
+  }
+}
