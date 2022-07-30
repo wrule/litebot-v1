e33eaadbebd8f2ae4b22ca08f1e4318c6cc13b2f
jimao <jimao@jimaodeMacBook-Pro.local>
2022-07-30 21:16:43 +0800

    顶层定义修改

--- utils/list/index.ts
+++ utils/list/index.ts
@@ -1,15 +1,15 @@
 
 export
 interface IList<T> {
-  Append(data: T): void | Promise<void>;
-
-  GetFirst(): (T | null) | Promise<(T | null)>;
-
-  UpdateFirst(data: T): void | Promise<void>;
+  Append(...data: T[]): void | Promise<void>;
 
   All(): T[] | Promise<T[]>;
 
   Length(): number | Promise<number>;
 
   Empty(): void | Promise<void>;
+
+  GetFirst(): (T | null) | Promise<(T | null)>;
+
+  SetFirst(data: T): void | Promise<void>;
 }
