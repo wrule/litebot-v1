c3f9f25f6ea61181022ea9c361d2c0c73da9132d
jimao <jimao@jimaodeMacBook-Pro.local>
2022-07-30 21:23:59 +0800

    提交

--- utils/list/memory_list.ts
+++ utils/list/memory_list.ts
@@ -5,23 +5,8 @@ class MemoryList<T>
 implements IList<T> {
   private list: T[] = [];
 
-  public Append(data: T) {
-    this.list.push(data);
-  }
-
-  public GetFirst() {
-    if (this.list.length > 0) {
-      return this.list[0];
-    }
-    return null;
-  }
-
-  public UpdateFirst(data: T) {
-    if (this.list.length > 0) {
-      this.list[0] = data;
-    } else {
-      this.list.push(data);
-    }
+  public Append(...data: T[]) {
+    this.list.push(...data);
   }
 
   public All() {
@@ -34,5 +19,20 @@ implements IList<T> {
 
   public Empty() {
     this.list = [];
+  }
+
+  public GetFirst() {
+    if (this.list.length > 0) {
+      return this.list[0];
+    }
+    return null;
+  }
+
+  public SetFirst(data: T) {
+    if (this.list.length > 0) {
+      this.list[0] = data;
+    } else {
+      this.list.push(data);
+    }
   }
 }
