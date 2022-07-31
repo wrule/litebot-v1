d50273efa458465a60316cea9f6caddd57196958
jimao <jimao@jimaodeMBP.lan>
2022-07-31 20:02:52 +0800

    watcher

--- index.ts
+++ index.ts
@@ -1,50 +1,19 @@
-import { TwoMaCross } from './robot/spot/two_ma_cross';
-import { TestSpot } from './executor/spot/test_spot';
-// import HistData from './data/ETH_USDT-2h.json';
-import { ArrayToKLine, IOHLCV } from './common/kline';
-import { IList } from './utils/list';
-import { JSONList } from './utils/list/json_list';
-import { MemoryList } from './utils/list/memory_list';
-import { Report } from './report';
-import path from 'path';
-import fs from 'fs';
+import { binance } from 'ccxt';
 import { JSONReport } from './report/json_report';
+import { KLineWatcher } from './watcher/kline_watcher';
+import secret from './.secret.json';
 
-// async function main() {
-//   const realData = ArrayToKLine(HistData);
-//   let maxValuation = -1;
-//   console.log('开始');
-//   const executor = new TestSpot(100, 0.001, false, 'USDT', 'ETH');
-//   for (let fast = 2; fast < 200; ++fast) {
-//     for (let slow = fast + 1; slow < 200; ++slow) {
-//       const robot = new TwoMaCross({ fast_ma: fast, slow_ma: slow }, executor);
-//       robot.BackTesting(realData);
-//       const valuation = await robot.Executor.Valuation(3026.85);
-//       if (valuation > maxValuation) {
-//         maxValuation = valuation;
-//         console.log(fast, slow, valuation);
-//       }
-//     }
-//   }
-//   console.log('结束');
-// }
-
-// main();
-
-async function testMain() {
-  const report = new JSONReport<any, IOHLCV, IOHLCV>('test_report');
-  const a = await report.Meta();
-  console.log(a);
-  report.AppendTransaction({a: 888} as any);
-  // await report.UpdateMeta({
-  //   name: '鸡毛的报告',
-  //   start_time: 0,
-  //   end_time: 11111,
-  //   params: {
-  //     fast_ma: 9,
-  //     slow_ma: 44,
-  //   },
-  // });
+function main() {
+  const client = new binance({
+    apiKey: secret.API_KEY,
+    secret: secret.SECRET_KEY,
+    enableRateLimit: true,
+  });
+  const watcher = new KLineWatcher(client, 1000, 'BTC/USDT', '1m', 10)
+  watcher.Subscribe((kline) => {
+    console.log(kline.length);
+  });
+  watcher.Start();
 }
 
-testMain();
+main();
