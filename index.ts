fc439512006881e1dad2598518def05d9771d035
jimao <jimao@jimaodeMBP.lan>
2022-07-31 20:32:04 +0800

    提交

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import { binance } from 'ccxt';
 import { JSONReport } from './report/json_report';
 import { KLineWatcher } from './watcher/kline_watcher';
 import secret from './.secret.json';
+import { TwoMaCross } from './robot/spot/two_ma_cross';
+import { BinanceSpot } from './executor/spot/binance_spot';
 
 function main() {
   const client = new binance({
@@ -9,9 +11,12 @@ function main() {
     secret: secret.SECRET_KEY,
     enableRateLimit: true,
   });
-  const watcher = new KLineWatcher(client, 1000, 'BTC/USDT', '1m', 10)
+  const watcher = new KLineWatcher(client, 1000, 'BTC/USDT', '1m', 10);
+  const report = new JSONReport('output/test_ma');
+  const executor = new BinanceSpot('BTC/USDT', client, 3, 'a.log');
+  const robot = new TwoMaCross({ fast_ma: 9, slow_ma: 44 }, executor);
   watcher.Subscribe((kline) => {
-    console.log(kline.length);
+    robot.CheckKLine(kline);
   });
   watcher.Start();
 }
