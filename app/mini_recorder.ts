c98327a3721d4a52a58448b06921461beb2a7585
jimao <jimao@jimaodeMacBook-Pro.local>
2022-08-08 10:43:59 +0800

    小记录器

--- app/mini_recorder.ts
+++ app/mini_recorder.ts
@@ -2,10 +2,18 @@ import { binance } from 'ccxt';
 import { TickerWatcher } from '../watcher/ticker_watcher';
 import { JSONList } from '../utils/list/json_list';
 
-const symbol = 'OP/USDT';
-const interval = 1000;
+function getArgs() {
+  if (process.argv.length === 4) {
+    const argvs = process.argv.slice(process.argv.length - 2);
+    const symbol = argvs[0].toUpperCase();
+    const interval = Number(argvs[1]);
+    return { symbol, interval };
+  }
+  throw '参数数量错误';
+}
 
 function main() {
+  const { symbol, interval } = getArgs();
   const client = new binance({ enableRateLimit: true });
   const watcher = new TickerWatcher(client, interval, symbol);
   const recorder = new JSONList(`output/${symbol.replace('/', '-')}.json`);
