2b3e72184448713a49a90ff08a4645483826f62d
jimao <jimao@jimaodeMacBook-Pro.local>
2022-08-09 11:13:44 +0800

    日志记录器

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,23 +1,30 @@
 import moment from 'moment';
 
 export
+type LogType = 'LOG' | 'ERROR' | 'WARN';
+
+export
 class Logger {
-  public meta() {
-    console.log(`[${moment(new Date()).format('YYYY-MM-DD HH:mm:ss')}]`);
+  public meta(type: LogType = 'LOG') {
+    console.log(`[${
+      moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
+    }] [${
+      type
+    }]:`);
   }
 
   public log(...data: any[]) {
-    this.meta();
+    this.meta('LOG');
     console.log(...data);
   }
 
   public error(...data: any[]) {
-    this.meta();
+    this.meta('ERROR');
     console.error(...data);
   }
 
   public warn(...data: any[]) {
-    this.meta();
+    this.meta('WARN');
     console.warn(...data);
   }
 }
