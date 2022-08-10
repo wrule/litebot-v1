be2c31e04e3e66cef39f823469955c9c72116bff
jimao <jimao@jimaodeMacBook-Pro.local>
2022-08-10 17:20:03 +0800

    fix: 提交代码

--- app/ma_crosser.ts
+++ app/ma_crosser.ts
@@ -7,6 +7,7 @@ import { DingTalk } from '../notifier/ding_talk';
 import dingConfig from '../.dingtalk.json';
 import { BinanceSpot } from '../executor/spot/binance_spot';
 import { TwoMaCross } from '../robot/spot/two_ma_cross';
+import { INotifier } from '../notifier';
 
 export
 interface IConfig {
@@ -22,37 +23,54 @@ interface IConfig {
 export
 class MACrosser
 extends App {
-  public constructor(
-    private readonly config: IConfig,
-  ) {
+  public constructor(private readonly config: IConfig) {
     super();
-  }
-
-  protected async run(...args: string[]) {
-    const ding = new DingTalk({
+    this.notifier = new DingTalk({
       secret: dingConfig.SECRET,
       access_token: dingConfig.ACCESS_TOKEN,
       at_mobiles: dingConfig.AT_MOBILES,
     });
-    const client = new binance({
+    this.client = new binance({
       apiKey: secret.API_KEY,
       secret: secret.SECRET_KEY,
       enableRateLimit: true,
     });
-    await client.loadMarkets();
-    const watcher = new KLineWatcher(
-      client,
+    this.watcher = new KLineWatcher(
+      this.client,
       this.config.interval,
       this.config.symbol,
       this.config.timeframe,
       100,
     );
-    const executor = new BinanceSpot(this.config.symbol, client, 3, 'tn.log');
-    const robot = new TwoMaCross({ fast_ma: this.config.fast, slow_ma: this.config.slow }, executor, undefined, ding);
-    watcher.Subscribe((kline_snapshot) => {
-      robot.CheckKLine(kline_snapshot.confirmed_kline, kline_snapshot.last);
+    this.executor = new BinanceSpot(
+      this.config.symbol,
+      this.client,
+      3,
+      'tn.log',
+    );
+    this.robot = new TwoMaCross(
+      { fast_ma: this.config.fast, slow_ma: this.config.slow },
+      this.executor,
+      undefined,
+      this.notifier,
+    );
+  }
+
+  private client!: binance;
+  private notifier!: INotifier;
+  private watcher!: KLineWatcher;
+  private executor!: BinanceSpot;
+  private robot!: TwoMaCross;
+
+  protected async run(...args: string[]) {
+    await this.client.loadMarkets();
+    this.watcher.Subscribe((kline_snapshot) => {
+      this.robot.CheckKLine(
+        kline_snapshot.confirmed_kline,
+        kline_snapshot.last,
+      );
     });
-    watcher.Start();
+    this.watcher.Start();
   }
 }
 
