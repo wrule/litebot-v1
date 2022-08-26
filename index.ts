f456571285fec28b93289540344cc335386e1d40
jimao <jimao@jimaodeMacBook-Pro.local>
2022-08-26 23:53:02 +0800

    明天开始优化性能

--- index.ts
+++ index.ts
@@ -6,6 +6,8 @@ import ETH_USDT_2h from './data/ETH_USDT-2h.json';
 import BTC_USDT_2h from './data/BTC_USDT-2h.json';
 import OP_USDT_2h from './data/OP_USDT-2h.json';
 import { TwoMaCross } from './robot/spot/two_ma_cross';
+import { JSONList } from './utils/list/json_list';
+import { ISnapshot } from './common/snapshot';
 
 async function main() {
   console.log('你好，世界');
@@ -14,6 +16,8 @@ async function main() {
     symbol: 'ETH/USDT',
     init_funds_amount: 100,
     fee: 0.001,
+    transaction_list: new JSONList<ITransaction>('output/test_tn.json'),
+    snapshot_list: new JSONList<ISnapshot>('output/test_sp.json'),
   });
   await spot.Reset();
   const robot = new TwoMaCross({ fast_ma: 9, slow_ma: 44 }, spot);
