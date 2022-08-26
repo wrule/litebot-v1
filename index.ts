25aeda774421af0e4e419f8af4dfe431c5619066
jimao <jimao@jimaodeMacBook-Pro.local>
2022-08-26 23:48:01 +0800

    回测代码

--- index.ts
+++ index.ts
@@ -1,20 +1,24 @@
 
 import { ITransaction } from './common/transaction';
 import { TestSpot } from './executor/spot/test_spot';
-import { JSONList } from './utils/list/json_list';
+import { ArrayToKLine } from './common/kline';
+import ETH_USDT_2h from './data/ETH_USDT-2h.json';
+import BTC_USDT_2h from './data/BTC_USDT-2h.json';
+import OP_USDT_2h from './data/OP_USDT-2h.json';
+import { TwoMaCross } from './robot/spot/two_ma_cross';
 
 async function main() {
   console.log('你好，世界');
+  const kline = ArrayToKLine(ETH_USDT_2h);
   const spot = new TestSpot({
-    symbol: 'BTC/USDT',
+    symbol: 'ETH/USDT',
     init_funds_amount: 100,
     fee: 0.001,
-    transaction_list: new JSONList<ITransaction>('output/ttn.json'),
   });
-  spot.Reset();
-  spot.BuyAll(10, 0);
-  spot.SellAll(20, 0);
-  console.log(spot.LatestSnapshot(1, 30));
+  await spot.Reset();
+  const robot = new TwoMaCross({ fast_ma: 9, slow_ma: 44 }, spot);
+  await robot.BackTesting(kline);
+  console.log(kline.length, spot.Valuation(1592));
 }
 
 main();
