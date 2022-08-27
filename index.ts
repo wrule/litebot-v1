8f0253ababe3216f9e7e61ea3b4df69484b9f13e
jimao <jimao@jimaodeMacBook-Pro.local>
2022-08-27 23:57:24 +0800

    迭代器测试

--- index.ts
+++ index.ts
@@ -2,9 +2,14 @@ import { VectorElement } from "./optimizer/vector";
 
 console.log('你好，世界');
 
-const ve = new VectorElement({ name: 'fast', range: [0, 10000], get_value: (current) => `${current / 100}%` });
+const ve = new VectorElement({ name: 'fast', range: [0, 100], get_value: (current) => `${current}%` });
 
-for(let a of ve) {
-  console.log(a);
-}
+const k = Array.from(ve);
+
+// for(let a of ve) {
+//   console.log(a);
+// }
+// for(let a of ve) {
+//   console.log(a);
+// }
 
