86235a1cd8eaaff583a9c6d06ed010cedc508f62
jimao <jimao@jimaodeMacBook-Pro.local>
2022-08-27 23:34:28 +0800

    迭代器实现

--- optimizer/vector.ts
+++ optimizer/vector.ts
@@ -3,46 +3,41 @@ export
 interface IVectorElementConfig {
   name: string;
   range: [number, number];
-  current?: number;
   get_value?: (current: number) => any;
 }
 
 export
-class VectorElement {
-  public constructor(_config: IVectorElementConfig) {
-    if (_config.range[1] < _config.range[0]) {
+class VectorElement
+implements Iterator<any> {
+  public constructor(private readonly config: IVectorElementConfig) {
+    if (this.config.range[1] < this.config.range[0]) {
       throw 'range[1]必须大于等于range[0]';
     }
-    if (
-      _config.current != null &&
-      (_config.current < _config.range[0] || _config.current > _config.range[1])
-    ) {
-      throw 'current必须大于等于range[0]且小于等于range[1]';
-    }
-    this.config = {
-      ..._config,
-      current: _config.current != null ? _config.current : _config.range[0],
-    };
+    this.current = this.config.range[0];
   }
 
-  private config!: IVectorElementConfig;
-
-  public get Range() {
-    return this.config.range;
-  }
-
-  public get Current(): number {
-    return this.config.current as number;
-  }
+  private current!: number;
 
   public get Value() {
     if (this.config.get_value)
-      return this.config.get_value(this.Current);
-    return this.Current;
+      return this.config.get_value(this.current);
+    return this.current;
   }
 
   public get KeyValue() {
     return { [this.config.name]: this.Value };
+  }
+
+  public next(): IteratorResult<any> {
+    let result: IteratorResult<any>;
+    if (this.current <= this.config.range[1]) {
+      result = { done: false, value: this.Value };
+      this.current++;
+    } else {
+      result = { done: true, value: null };
+      this.current = this.config.range[0];
+    }
+    return result;
   }
 }
 
