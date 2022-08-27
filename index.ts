1391dad974205398885542d9eae37e723c9b4de0
jimao <jimao@jimaodeMacBook-Pro.local>
2022-08-27 21:15:17 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -12,9 +12,18 @@ import { ISnapshot } from './common/snapshot';
 async function main() {
   const kline = ArrayToKLine(ETH_USDT_2h);
   const spot = new TestSpot({ symbol: 'ETH/USDT', init_funds_amount: 100, fee: 0.001 });
-  const robot = await new TwoMaCross({ params: { fast_ma: 9, slow_ma: 44 }, executor: spot }).Reset();
-  await robot.BackTesting(kline);
-  console.log(kline.length, spot.Valuation(1592));
+  let max = -1, max_info = '';
+  for (let i = 2; i < 100; ++i) {
+    for (let j = i + 1; j < 100; ++j) {
+      const robot = await new TwoMaCross({ params: { fast_ma: i, slow_ma: j }, executor: spot }).Reset();
+      await robot.BackTesting(kline);
+      if (spot.Valuation(1592) > max) {
+        max = spot.Valuation(1592);
+        max_info = `${i}:${j}`;
+      }
+      console.log(i, j, spot.Valuation(1592), max, max_info);
+    }
+  }
 }
 
 main();
