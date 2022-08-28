1c810f82ce4d1e85b1475816a2feaea8109d7fbc
jimao <jimao@jimaodeMacBook-Pro.local>
2022-08-28 11:53:34 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ console.log('你好，世界');
 const v = new Vector([
   { name: 'fast', range: [0, 100] },
   { name: 'slow', range: [0, 100] },
+  { name: 'k', range: [0, 10] },
 ]);
 
 console.log(v.RandomKeyValue);
