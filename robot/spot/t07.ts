d71860205f035c582be50f7f5a880441cd2bf3e5
jimao <jimao@jimaodeMacBook-Pro.local>
2022-08-29 18:02:58 +0800

    t07的策略模板

---
+++ robot/spot/t07.ts
@@ -0,0 +1,44 @@
+import { IOHLCV } from '../../common/kline';
+import { ISpotRobotConfig, SpotRobot } from '.';
+
+export
+interface IParams {
+  macd_fast_ma: number;
+  macd_slow_ma: number;
+  macd_diff_ma: number;
+  cross_limit: number;
+  sold_candles: number;
+  atr: number;
+  atr_multiplier: number;
+}
+
+export
+interface ITestData
+extends IOHLCV {
+  buy?: boolean;
+  sell?: boolean;
+}
+
+export
+class T07
+extends SpotRobot<IParams, IOHLCV, ITestData> {
+  public constructor(config: ISpotRobotConfig<IParams, IOHLCV, ITestData>) {
+    super(config);
+  }
+
+  public get KLineReadyLength() {
+    return 0;
+  }
+
+  public async checkKLine(confirmed_kline: IOHLCV[], last_confirmed: IOHLCV) {
+
+  }
+
+  public GenerateTestData(real_data: IOHLCV[]): ITestData[] {
+    return [];
+  }
+
+  protected checkTestData(data: ITestData): void | Promise<void> {
+
+  }
+}
