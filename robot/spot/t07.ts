d2f70b3a54bcf20d13f999336904be8a1b50bded
jimao <jimao@jimaodeMacBook-Pro.local>
2022-08-29 18:47:59 +0800

    t07的macd指标

--- robot/spot/t07.ts
+++ robot/spot/t07.ts
@@ -27,6 +27,23 @@ extends SpotRobot<IParams, IOHLCV, ITestData> {
     super(config);
   }
 
+  private macd(closes: number[], params: {
+    macd_fast_ma: number,
+    macd_slow_ma: number,
+    macd_diff_ma: number,
+  }) {
+    let result: any = null;
+    tulind.indicators.macd.indicator(
+      [closes],
+      [params.macd_fast_ma, params.macd_slow_ma, params.macd_diff_ma],
+      (error: any, data: any) => {
+        if (error) throw error;
+        result = data;
+      },
+    );
+    return result;
+  }
+
   public get KLineReadyLength() {
     return 0;
   }
@@ -36,7 +53,8 @@ extends SpotRobot<IParams, IOHLCV, ITestData> {
   }
 
   public GenerateTestData(real_data: IOHLCV[]): ITestData[] {
-    return [];
+    const macd = this.macd(real_data.map((item) => item.close), this.config.params);
+    return macd;
   }
 
   protected checkTestData(data: ITestData): void | Promise<void> {
