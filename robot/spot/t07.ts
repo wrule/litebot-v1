7bbb2770d262ebf7beafb87b46b28623642eb7f1
jimao <jimao@jimaodeMacBook-Pro.local>
2022-08-30 11:36:27 +0800

    macd测试通过

--- robot/spot/t07.ts
+++ robot/spot/t07.ts
@@ -1,6 +1,7 @@
 import tulind from 'tulind';
 import { IOHLCV } from '../../common/kline';
 import { ISpotRobotConfig, SpotRobot } from '.';
+import moment from 'moment';
 
 export
 interface IParams {
@@ -18,6 +19,7 @@ interface ITestData
 extends IOHLCV {
   buy?: boolean;
   sell?: boolean;
+  price?: boolean;
 }
 
 export
@@ -35,6 +37,18 @@ extends SpotRobot<IParams, IOHLCV, ITestData> {
     super(config);
   }
 
+  private macd_start(params: {
+    macd_fast_ma: number,
+    macd_slow_ma: number,
+    macd_diff_ma: number,
+  }) {
+    return tulind.indicators.macd.start([
+      params.macd_fast_ma,
+      params.macd_slow_ma,
+      params.macd_diff_ma,
+    ]);
+  }
+
   private macd(closes: number[], params: {
     macd_fast_ma: number,
     macd_slow_ma: number,
@@ -42,7 +56,7 @@ extends SpotRobot<IParams, IOHLCV, ITestData> {
   }): IMACDResult {
     const result: IMACDResult = { start: 0, dif: [], dea: [], macd: [], };
     const options = [params.macd_fast_ma, params.macd_slow_ma, params.macd_diff_ma];
-    result.start = tulind.indicators.macd.start(options);
+    result.start = this.macd_start(params);
     tulind.indicators.macd.indicator([closes], options, (error: any, data: any) => {
         if (error) throw error;
         result.dif = Array(result.start).fill(null).concat(data[0]);
@@ -54,7 +68,9 @@ extends SpotRobot<IParams, IOHLCV, ITestData> {
   }
 
   public get KLineReadyLength() {
-    return 0;
+    return Math.max(
+      this.macd_start(this.config.params),
+    ) + 2;
   }
 
   public async checkKLine(confirmed_kline: IOHLCV[], last_confirmed: IOHLCV) {
@@ -62,10 +78,27 @@ extends SpotRobot<IParams, IOHLCV, ITestData> {
   }
 
   public GenerateTestData(real_data: IOHLCV[]): ITestData[] {
-    const macd = this.macd(real_data.map((item) => item.close), this.config.params);
-    console.log(macd.dif.length);
-    console.log(macd.dea.length);
-    console.log(macd.macd.length);
+    const { macd } = this.macd(real_data.map((item) => item.close), this.config.params);
+    console.log(macd.length);
+    console.log(this.KLineReadyIndex);
+    const a = real_data.map((item, index) => {
+      const result: ITestData = { ...item };
+      if (index >= this.KLineReadyIndex) {
+        const macd_last = macd[index];
+        const macd_prev = macd[index - 1];
+        if (macd_last > 0 && macd_prev <= 0) {
+          result.buy = true;
+        }
+        if (macd_last < 0 && macd_prev >= 0) {
+          result.sell = true;
+        }
+      }
+      return result;
+    });
+    const b = a
+      .filter((item) => item.buy || item.sell)
+      .map((item) => ({ ...item, time: moment(new Date(item.time)).format('YYYY-MM-DD HH:mm:ss') }));
+    console.log(b.slice(b.length - 3));
     return [];
   }
 
