4b5bc8deb5bd8406536a2e927e75800116573629
jimao <jimao@jimaodeMacBook-Pro.local>
2022-08-31 14:42:20 +0800

    回测

--- index.ts
+++ index.ts
@@ -1,23 +1,30 @@
 import moment from 'moment';
 import { ArrayToKLine } from './common/kline';
-import ETH_USDT_1d from './data/ETH_USDT-1d.json';
+import ETH_USDT_2h from './data/ETH_USDT-2h.json';
+import { TestSpot } from './executor/spot/test_spot';
 import { T07 } from './robot/spot/t07';
 
-const robot = new T07({
-  params: {
-    macd_fast_ma: 9,
-    macd_slow_ma: 44,
-    macd_diff_ma: 9,
-    cross_limit: 2,
-    sold_candles: 9,
-    atr: 7,
-    atr_multiplier: 0.5,
-  },
-  executor: null as any,
-});
+async function main() {
+  const executor = new TestSpot({
+    symbol: 'ETH/USDT',
+    fee: 0.001,
+    init_funds_amount: 100,
+  });
+  const robot = new T07({
+    params: {
+      macd_fast_ma: 9,
+      macd_slow_ma: 44,
+      macd_diff_ma: 9,
+      cross_limit: 3,
+      sold_candles: 5,
+      atr: 7,
+      atr_multiplier: 0.5,
+    },
+    executor: executor,
+  });
+  const kline = ArrayToKLine(ETH_USDT_2h);
+  await robot.BackTesting(kline);
+  console.log(executor.FundBalance, executor.AssetBalance);
+}
 
-const kline = ArrayToKLine(ETH_USDT_1d);
-const result = robot.GenerateTestData(kline);
-const v_result = result.filter((item) => item.buy || item.sell);
-// console.log(v_result.slice(v_result.length - 10));
-console.log(v_result.length);
+main();
