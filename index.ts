0ef61046ca9c202051eccd849dcf96dcb598914f
jimao <jimao@jimaodeMacBook-Pro.local>
2022-08-31 14:19:50 +0800

    验证信号

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import { ArrayToKLine } from './common/kline';
 import ETH_USDT_1d from './data/ETH_USDT-1d.json';
 import { T07 } from './robot/spot/t07';
@@ -18,4 +19,8 @@ const robot = new T07({
 const kline = ArrayToKLine(ETH_USDT_1d);
 const result = robot.GenerateTestData(kline);
 const v_result = result.filter((item) => item.buy || item.sell);
-console.log(result.slice(result.length - 10));
+// console.log(v_result.slice(v_result.length - 10));
+console.log(v_result.slice(v_result.length - 5).map((item) => ({
+  ...item,
+  time: moment(new Date(item.time)).format('YYYY-MM-DD HH:mm:ss'),
+})));
