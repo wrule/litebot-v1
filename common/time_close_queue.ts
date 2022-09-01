99157e7aa675868221be512308808f655c3a1dfb
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-01 13:59:32 +0800

    TimeCloseQueue

---
+++ common/time_close_queue.ts
@@ -0,0 +1,22 @@
+import { ITimeClose } from './kline';
+
+export
+abstract class TimeCloseQueue<T extends ITimeClose> {
+  public constructor(private readonly limit: number) {
+    if (this.limit < 1) throw 'limit必须大于等于1';
+  }
+
+  private queue: T[] = [];
+
+  public get Length() {
+    return this.queue.length;
+  }
+
+  public Append(item: T) {
+    this.queue.push(item);
+    const diff = this.queue.length - this.limit;
+    if (diff > 0) {
+      this.queue.splice(0, diff);
+    }
+  }
+}
