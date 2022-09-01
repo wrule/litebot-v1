ea0d4e4c2a935653db28c9ee98829411b35057bf
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-01 22:10:36 +0800

    参数空间部分代码测试完成

--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
 
-console.log('你好，世界');
+import { ParamSpace, ParamsSpace } from './optimizer/params_space';
+
+const s = new ParamsSpace([
+  { name: 'fast', range: [0, 10], },
+  { name: 'slow', range: [0, 20], },
+  { name: 'sex', range: [0, 1], get_value: (current) => ['女', '男'][current], },
+]);
+
+console.log(s.RandomKeyValues());
