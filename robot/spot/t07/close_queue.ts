51250b889018dac7d4ffac3c89ddcc208ef653fc
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-01 16:10:01 +0800

    提交代码

--- robot/spot/t07/close_queue.ts
+++ robot/spot/t07/close_queue.ts
@@ -1,9 +1,10 @@
 // 2022年09月01日16:02:44
+import { IOHLCV } from '../../../common/kline';
 import { OHLCV_Queue } from '../../../common/time_close_queue';
 
 export
 class CloseQueue
-extends OHLCV_Queue {
+extends OHLCV_Queue<IOHLCV> {
   public constructor(config: { close_candles: number }) {
     super(config.close_candles);
   }
