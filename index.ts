7ac7fe41800602ab3b68d5adc60b91a752333a68
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-02 16:48:35 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -2,14 +2,12 @@ import { OptimizerRanking, Optimizer } from './optimizer';
 import { Logger } from './utils/logger';
 
 async function main() {
-  console.log('你好，世界');
   const opt = new Optimizer({
     space: [
       { name: 'a', range: [-1000, 1000], },
       { name: 'b', range: [-1000, 1000], },
-      { name: 'c', range: [-1000, 1000], },
     ],
-    objective_function: (input: any) => ({ output: input.a * input.a + input.b * input.b }),
+    objective_function: (input: any) => ({ output: (input.a * input.a) + (input.b * input.b) }),
     loss_function: (output) => 1 / output.output,
     logger: new Logger(),
   });
