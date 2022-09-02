e220896142c19489e4627967973342b6e8a9e40b
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-02 15:24:29 +0800

    测试代码

--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
+import { OptimizerRanking } from './optimizer';
 
-import { OptimizerRanking } from './optimizer'
-
-const ranking = new OptimizerRanking({ ranking_limit: 10, });
-console.log(ranking.TryAdd({ input: { }, loss: 5, output: 0, }));
-console.log(ranking.TryAdd({ input: { }, loss: 1, output: 0, }));
-console.log(ranking.TryAdd({ input: { }, loss: 0.1, output: 0, }));
-console.log(ranking.TryAdd({ input: { }, loss: 3, output: 0, }));
-console.log(ranking.TryAdd({ input: { }, loss: 10, output: 0, }));
+const ranking = new OptimizerRanking({ ranking_limit: 2 });
+console.log(ranking.TryAdd({ input: { }, output: 1, loss: 0.1 }));
+console.log(ranking.TryAdd({ input: { }, output: 1, loss: 1 }));
+console.log(ranking.TryAdd({ input: { }, output: 1, loss: 2 }));
+console.log(ranking.TryAdd({ input: { }, output: 1, loss: 0.5 }));
+console.log(ranking.TryAdd({ input: { }, output: 1, loss: 0.2 }));
 console.log(ranking.Ranking);
