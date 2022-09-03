06e974f91f2c0eab180b471d7b29135934db081f
jimao <jimao@jimaodeMBP.lan>
2022-09-03 20:58:24 +0800

    更新示例代码

--- example/back_testing.ts
+++ example/back_testing.ts
@@ -1,11 +1,11 @@
 import { ArrayToKLine } from '../common/kline';
-import { TestSpot } from '../executor/spot/test_spot';
-import { TwoMaCross } from '../robot/spot/two_ma_cross';
 import ETH_USDT_2h from '../data/ETH_USDT-2h.json';
+import { TestSpot } from '../executor/spot/test_spot';
+import { IParams, TwoMaCross } from '../robot/spot/two_ma_cross';
 
 const kline = ArrayToKLine(ETH_USDT_2h);
 
-async function back_testing(params: any) {
+async function back_testing(params: IParams) {
   const executor = new TestSpot({ symbol: 'ETH/USDT', fee: 0.001, init_funds_amount: 100, });
   const robot = new TwoMaCross({ params: { fast_ma: params.fast_ma, slow_ma: params.slow_ma, }, executor, });
   await robot.BackTesting(kline);
--- example/optimization.ts
+++ example/optimization.ts
@@ -7,7 +7,7 @@ import { Logger } from '../utils/logger';
 
 const kline = ArrayToKLine(ETH_USDT_2h);
 
-async function back_testing(params: any): Promise<IFunctionOutput<unknown>> {
+async function back_testing(params: IParams): Promise<IFunctionOutput<any>> {
   const executor = new TestSpot({ symbol: 'ETH/USDT', fee: 0.001, init_funds_amount: 100, });
   const robot = new TwoMaCross({ params: { fast_ma: params.fast_ma, slow_ma: params.slow_ma, }, executor, });
   await robot.BackTesting(kline);
@@ -18,18 +18,17 @@ async function main() {
   const opt = new Optimizer({
     space: [
       { name: 'fast_ma', range: [2, 100], },
-      { name: 'slow_ma', range: [2, 100], }
+      { name: 'slow_ma', range: [2, 100], },
     ],
     objective_function: back_testing,
     loss_function: (output) => 1 / output.output,
-    input_mapper: (input: any) => ({
+    input_mapper: (input) => ({
       fast_ma: input.fast_ma < input.slow_ma ? input.fast_ma : input.slow_ma,
       slow_ma: input.fast_ma < input.slow_ma ? input.slow_ma : input.fast_ma,
     }),
     logger: new Logger(),
   });
   opt.Search();
-  // console.log(await back_testing({ fast_ma: 9, slow_ma: 44, }));
 }
 
 main();
