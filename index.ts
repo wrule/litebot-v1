f7d6546b01220423b75edd8b82108fc9da66e159
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-03 14:36:30 +0800

    双均线机器人回测

--- index.ts
+++ index.ts
@@ -1,20 +1,15 @@
-import { Optimizer } from './optimizer';
-import { Logger } from './utils/logger';
+import { ArrayToKLine } from './common/kline';
+import ETH_USDT_2h from './data/ETH_USDT-2h.json';
+import { TestSpot } from './executor/spot/test_spot';
+import { TwoMaCross } from './robot/spot/two_ma_cross';
 
 async function main() {
-  const opt = new Optimizer({
-    space: [
-      { name: 'a', range: [-1000, 1000], },
-      { name: 'b', range: [-1000, 1000], },
-    ],
-    objective_function: (input: any) => ({ output: (input.a * input.a) + (input.b * input.b) }),
-    loss_function: (output) => 1 / output.output,
-    ranking_limit: 10,
-    iterations: 10000000,
-    logger: new Logger(),
-  });
-  const ranking = await opt.Search();
-  console.log(ranking);
+  const kline = ArrayToKLine(ETH_USDT_2h);
+  const executor = new TestSpot({ symbol: 'ETH/USDT', fee: 0.001, init_funds_amount: 100, });
+  const robot = new TwoMaCross({ params: { fast_ma: 9, slow_ma: 44, }, executor, });
+  await robot.BackTesting(kline);
+  console.log(executor.Valuation(1600));
+  console.log(executor.Valuation(1600));
 }
 
 main();
