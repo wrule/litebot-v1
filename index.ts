6d3fae8825b4de024cf2e8b790152942c4243171
jimao <jimao@jimaodeMBP.lan>
2022-09-03 21:50:53 +0800

    srsi指标还没有验证出来

--- index.ts
+++ index.ts
@@ -1,34 +1,19 @@
+import { SRSI_Martin } from './robot/spot/srsi_martin';
+import ETH_USDT_1h from './data/ETH_USDT-1h.json';
 import { ArrayToKLine } from './common/kline';
-import ETH_USDT_2h from './data/ETH_USDT-2h.json';
-import { TestSpot } from './executor/spot/test_spot';
-import { IFunctionOutput, Optimizer } from './optimizer';
-import { IParams, TwoMaCross } from './robot/spot/two_ma_cross';
-import { Logger } from './utils/logger';
 
-const kline = ArrayToKLine(ETH_USDT_2h as any[]);
-
-async function back_testing(params: IParams): Promise<IFunctionOutput<any>> {
-  const executor = new TestSpot({ symbol: 'ETH/USDT', fee: 0.001, init_funds_amount: 100, });
-  const robot = new TwoMaCross({ params: { fast_ma: params.fast_ma, slow_ma: params.slow_ma, }, executor, });
-  await robot.BackTesting(kline);
-  return { output: executor.Valuation(1600), };
-}
+const kline = ArrayToKLine(ETH_USDT_1h);
 
 async function main() {
-  const opt = new Optimizer({
-    space: [
-      { name: 'fast_ma', range: [2, 100], },
-      { name: 'slow_ma', range: [2, 100], },
-    ],
-    objective_function: back_testing,
-    loss_function: (output) => 1 / output.output,
-    input_mapper: (input) => ({
-      fast_ma: input.fast_ma < input.slow_ma ? input.fast_ma : input.slow_ma,
-      slow_ma: input.fast_ma < input.slow_ma ? input.slow_ma : input.fast_ma,
-    }),
-    logger: new Logger(),
+  const robot = new SRSI_Martin({
+    params: {
+      fast_size: 12,
+      slow_size: 26,
+      smoothing_size: 9,
+    },
+    executor: null as any,
   });
-  opt.Search();
+  robot.GenerateTestData(kline);
 }
 
 main();
--- robot/spot/srsi_martin/index.ts
+++ robot/spot/srsi_martin/index.ts
@@ -9,8 +9,9 @@ import { Report } from '@/report';
 
 export
 interface IParams {
-  fast_ma: number;
-  slow_ma: number;
+  fast_size: number;
+  slow_size: number;
+  smoothing_size: number;
 }
 
 export
@@ -21,93 +22,53 @@ extends IOHLCV {
 }
 
 export
-class TwoMaCross
+class SRSI_Martin
 extends SpotRobot<IParams, IOHLCV, ITestData> {
   public constructor(config: ISpotRobotConfig<IParams, IOHLCV, ITestData>) {
     super(config);
   }
 
-  private sma(closes: number[], size: number) {
-    let result: number[] = [];
-    tulind.indicators.sma.indicator(
-      [closes],
-      [size],
+  private srsi(close: number[]) {
+    let rsi: number[] = [];
+    tulind.indicators.rsi.indicator(
+      [close],
+      [27],
       (error: any, data: any) => {
         if (error) {
           throw error;
         }
-        result = Array(size - 1).fill(null).concat(data[0]);
+        rsi = data[0];
       },
     );
-    return result;
-  }
-
-  private message(tn: ITransaction, prev_diff: number, last_diff: number) {
-    this.SendMessage(`[${
-      moment(new Date(tn.transaction_time)).format('HH:mm:ss')
-    }  ${
-      { 'BUY': '买', 'SELL': '卖' }[tn.action as string]
-    }  ${
-      `${tn.in_amount}${tn.in_name} =(${tn.price})=> ${tn.out_amount}${tn.out_name}`
-    }]\n前差: ${prev_diff}  现差: ${last_diff}\n走单耗时: ${(tn.transaction_time - tn.request_time) / 1000}秒`);
-  }
-
-  public get KLineReadyLength() {
-    return Math.max(this.config.params.fast_ma, this.config.params.slow_ma) + 1;
+    console.log(rsi.length);
+    console.log(tulind.indicators.stoch);
+    let k: number[] = [];
+    let d: number[] = [];
+    tulind.indicators.stoch.indicator(
+      [rsi, rsi, rsi,],
+      [4, 33, 21],
+      (error: any, data: any) => {
+        if (error) {
+          throw error;
+        }
+        k = data[0];
+        d = data[1];
+      },
+    );
+    console.log(k.slice(k.length - 15));
+    console.log(d.slice(d.length - 15));
+    // console.log(d.length);
   }
 
   //#region 实盘运行接口实现
-  protected async checkKLine(confirmed_kline: KLine, last_confirmed: IOHLCV) {
-    try {
-      const closes = confirmed_kline.map((item) => item.close);
-      const fast_line = this.sma(closes, this.config.params.fast_ma);
-      const slow_line = this.sma(closes, this.config.params.slow_ma);
-      const prev_diff = fast_line[fast_line.length - 2] - slow_line[slow_line.length - 2];
-      const last_diff = fast_line[fast_line.length - 1] - slow_line[slow_line.length - 1];
-      this.logger.log(
-        '时间', moment(new Date(last_confirmed.time)).format('YYYY-MM-DD HH:mm:ss'),
-        '前差', prev_diff,
-        '现差', last_diff,
-      );
-      if (this.gold_cross_line(fast_line, slow_line)) {
-        const tn = await this.config.executor.BuyAll(last_confirmed.close);
-        this.message(tn, prev_diff, last_diff);
-      } else if (this.dead_cross_line(fast_line, slow_line)) {
-        const tn = await this.config.executor.SellAll(last_confirmed.close);
-        this.message(tn, prev_diff, last_diff);
-      }
-    } catch (e) {
-      this.logger.error(e);
-    }
-  }
   //#endregion
 
   //#region 回测运行接口实现
-  public GenerateTestData(kline: KLine): ITestData[] {
-    const closes = kline.map((item) => item.close);
-    const fast_line = this.sma(closes, this.config.params.fast_ma);
-    const slow_line = this.sma(closes, this.config.params.slow_ma);
-    return kline.map((item, index) => {
-      const result: ITestData = { ...item };
-      if (index >= this.KLineReadyIndex) {
-        const fast_last = fast_line[index], slow_last = slow_line[index];
-        const fast_prev = fast_line[index - 1], slow_prev = slow_line[index - 1];
-        if (this.gold_cross(fast_prev, slow_prev, fast_last, slow_last)) {
-          result.buy = true;
-        } else if (this.dead_cross(fast_prev, slow_prev, fast_last, slow_last)) {
-          result.sell = true;
-        }
-      }
-      return result;
-    });
-  }
-
-  protected async checkTestData(data: ITestData) {
-    if (data.buy) {
-      await this.config.executor.BuyAll(data.close, data.time);
-    } else if (data.sell) {
-      await this.config.executor.SellAll(data.close, data.time);
-    }
+  public GenerateTestData(real_data: IOHLCV[]): ITestData[] {
+    const last = real_data[real_data.length - 1];
+    console.log(moment(new Date(last.time)).format('YYYY-MM-DD HH:mm:ss'));
+    this.srsi(real_data.map((item) => item.close));
+    return [];
   }
   //#endregion
 }
