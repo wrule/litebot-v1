a412b4c43e6ffde896b49f6c41cd6ae37edb80b0
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-04 19:52:35 +0800

    rsi果然有问题

--- index.ts
+++ index.ts
@@ -1,34 +1,24 @@
-import { IParams, SRSI_Martin } from './robot/spot/srsi_martin';
-import ETH_USDT_1h from './data/ETH_USDT-30m.json';
-import { ArrayToKLine } from './common/kline';
-import { TestSpot } from './executor/spot/test_spot';
-import { Optimizer } from './optimizer';
-import { Logger } from './utils/logger';
+import tulind from 'tulind';
 
-const kline = ArrayToKLine(ETH_USDT_1h as any[]);
-
-console.log('30m', kline.length);
-
-async function back_testing(params: IParams) {
-  const executor = new TestSpot({ symbol: 'ETH/USDT', fee: 0.001, init_funds_amount: 100 });
-  const robot = new SRSI_Martin({ params, executor });
-  await robot.BackTesting(kline);
-  return { output: executor.Valuation(1600) }
+function rsi(data: number[], size: number) {
+  let rsi: number[] = [];
+  tulind.indicators.rsi.indicator(
+    [data],
+    [size],
+    (error: any, result: any) => {
+      if (error) throw error;
+      rsi = result[0];
+    },
+  );
+  return rsi;
 }
 
-async function main() {
-  const opt = new Optimizer({
-    space: [
-      { name: 'rsi_size', range: [13, 13], },
-      { name: 'k_size', range: [30, 34], },
-      { name: 'd_size', range: [43, 47], },
-      { name: 'stoch_size', range: [43, 47], },
-    ],
-    objective_function: back_testing,
-    loss_function: (output) => 1 / output.output,
-    logger: new Logger(),
-  });
-  opt.Search();
+function main() {
+  console.log('你好，世界');
+  console.log(rsi([1, 2, 1, 9, 2, 3, 8, 8], 3));
+  console.log(rsi([1, 2, 1, 9, 2, 3, 8, 8, 7], 3));
+  console.log(rsi([1, 2, 1, 9, 2, 3, 8, 8, 7, 8], 3));
+  console.log(rsi([1, 2, 1, 9, 2, 3, 8, 8, 7, 8, 6], 3));
 }
 
 main();
