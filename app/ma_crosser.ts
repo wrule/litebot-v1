26ee89e277092c8d9a7812a657993fa8462eba0c
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-06 13:43:32 +0800

    机器人重置

--- app/ma_crosser.ts
+++ app/ma_crosser.ts
@@ -72,6 +72,7 @@ extends App {
   private watcher!: KLineWatcher;
 
   protected async run(...args: string[]) {
+    await this.robot.Reset();
     this.logger.log('加载市场');
     await this.client.loadMarkets();
     this.logger.log('加载完成');
