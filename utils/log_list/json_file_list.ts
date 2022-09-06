185f1279f32220b3fbc66b2abb0954a1d4441c91
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-06 19:20:18 +0800

    json_file_list

--- utils/log_list/json_file_list.ts
+++ utils/log_list/json_file_list.ts
@@ -12,4 +12,35 @@ implements ILogList<T> {
       await fsPromise.appendFile(this.file_path, JSON.stringify(data[i], null, 2) + ',\n', 'utf-8');
     }
   }
+
+  public async All() {
+    // if (!fsPromise. (this.file_path)) {
+    //   fs.writeFileSync(this.file, '', 'utf-8');
+    // }
+    let json_text = await fsPromise.readFile(this.file_path, 'utf-8');
+    json_text = `[${json_text.substring(0, json_text.length - 2)}]`;
+    return JSON.parse(json_text) as T[];
+  }
+
+  public async Length() {
+    return (await this.All()).length;
+  }
+
+  public async Empty() {
+    await fsPromise.writeFile(this.file_path, '', 'utf-8');
+  }
+
+  public async GetFirst() {
+    const list = await this.All();
+    if (list.length < 1) return null;
+    return list[0];
+  }
+
+  public async SetFirst(data: T) {
+    const list = await this.All();
+    if (list.length < 1) list.push(data);
+    else list[0] = data;
+    await this.Empty();
+    await this.Append(...list);
+  }
 }
