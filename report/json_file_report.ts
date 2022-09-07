93e39b09331e4952ca035acdae6f3ebc72b01c30
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-07 22:37:55 +0800

    json_file_list

--- report/json_file_report.ts
+++ report/json_file_report.ts
@@ -1,24 +1,27 @@
+import fs from 'fs';
+import path from 'path';
 import { ITimeClose } from '../common/kline';
 import { ISnapshot } from '../common/snapshot';
 import { ITransaction } from '../common/transaction';
-import { MemoryList } from '../utils/log_list/memory_list';
+import { JSONFileList } from '../utils/log_list/json_file_list';
 import { Report, IReportMetaData } from '.';
 
 export
-class MemoryReport<
+class JSONFileReport<
   Params,
   HistoricalData extends ITimeClose,
   SignalData extends HistoricalData,
   Snapshot extends ISnapshot,
 >
 extends Report<Params, HistoricalData, SignalData, Snapshot> {
-  public constructor() {
+  public constructor(report_dir_path: string) {
+    fs.mkdirSync(report_dir_path, { recursive: true });
     super({
-      meta_data: new MemoryList<IReportMetaData<Params, Snapshot>>(),
-      historical_data: new MemoryList<HistoricalData>(),
-      signal_data: new MemoryList<SignalData>(),
-      transactions: new MemoryList<ITransaction>(),
-      snapshots: new MemoryList<Snapshot>(),
+      meta_data: new JSONFileList<IReportMetaData<Params, Snapshot>>(path.join(report_dir_path, 'meta_data.json')),
+      historical_data: new JSONFileList<HistoricalData>(path.join(report_dir_path, 'historical_data.json')),
+      signal_data: new JSONFileList<SignalData>(path.join(report_dir_path, 'signal_data.json')),
+      transactions: new JSONFileList<ITransaction>(path.join(report_dir_path, 'transactions.json')),
+      snapshots: new JSONFileList<Snapshot>(path.join(report_dir_path, 'snapshots.json')),
     });
   }
 }
