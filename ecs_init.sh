28f1b9069bd21a3c1741e76fa2a746653b0f57e3
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-07 12:04:25 +0800

    ecs初始化脚本

---
+++ ecs_init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt update
+apt install wget curl vim zsh git screen
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install 14
+npm install --global yarn
+npm install -g typescript
