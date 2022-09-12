c3f03b3fe5ca7473d67f7e868dc447ffa992d2b0
jimao <jimao@jimaodeMBP.lan>
2022-09-12 14:49:03 +0800

    提交代码

--- example/optimization.ts
+++ example/optimization.ts
@@ -4,8 +4,8 @@ import { IFunctionOutput, Optimizer } from '../optimizer';
 import { IParams, TwoMaCross } from '../robot/spot/two_ma_cross';
 import { Logger } from '../utils/logger';
 
-const LINK_USDT_30m = require('../../data/LINK_USDT-30m.json');
-const kline = ArrayToKLine(LINK_USDT_30m);
+const ohlcv_data = require('../../data/LINK_USDT-15m.json');
+const kline = ArrayToKLine(ohlcv_data);
 
 async function back_testing(params: IParams): Promise<IFunctionOutput<any>> {
   const executor = new TestSpot({ symbol: 'LINK/USDT', fee: 0.001, init_funds_amount: 100, });
@@ -15,22 +15,20 @@ async function back_testing(params: IParams): Promise<IFunctionOutput<any>> {
 }
 
 async function main() {
-  const a = await back_testing({ fast_size: 9, slow_size: 44 });
-  console.log(a);
-  // const opt = new Optimizer({
-  //   space: [
-  //     { name: 'fast_ma', range: [2, 100], },
-  //     { name: 'slow_ma', range: [2, 100], },
-  //   ],
-  //   objective_function: back_testing,
-  //   loss_function: (output) => 1 / output.output,
-  //   input_mapper: (input) => ({
-  //     fast_ma: input.fast_ma < input.slow_ma ? input.fast_ma : input.slow_ma,
-  //     slow_ma: input.fast_ma < input.slow_ma ? input.slow_ma : input.fast_ma,
-  //   }),
-  //   logger: new Logger(),
-  // });
-  // opt.Search();
+  const opt = new Optimizer({
+    space: [
+      { name: 'fast_size', range: [2, 100], },
+      { name: 'slow_size', range: [2, 100], },
+    ],
+    objective_function: back_testing,
+    loss_function: (output) => 1 / output.output,
+    input_mapper: (input) => ({
+      fast_size: input.fast_size < input.slow_size ? input.fast_size : input.slow_size,
+      slow_size: input.fast_size < input.slow_size ? input.slow_size : input.fast_size,
+    }),
+    logger: new Logger(),
+  });
+  opt.Search();
 }
 
 main();
