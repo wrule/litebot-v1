7bb75db03f3a26adf630bab6c000e4cdf8c6db8f
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-13 12:37:52 +0800

    提交代码

--- shell/ecs_ubuntu_init.sh
+++ shell/ecs_ubuntu_init.sh
@@ -2,8 +2,3 @@
 apt update
 apt install curl screen
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-source ~/.bashrc
-nvm install 14
-npm install --global yarn
-npm install --global typescript
-npm install
---
+++ shell/node_init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+nvm install 14
+npm install --global yarn
+npm install --global typescript
+npm install
