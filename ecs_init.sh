4e5ee5bf2df1e71a47eb3f2fbbf22687adaa2280
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-13 12:23:59 +0800

    ecs初始化脚本

--- ecs_init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt update
-apt install wget curl vim zsh git screen
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
--- ecs_ubuntu_init.sh
+++ ecs_ubuntu_init.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/zsh
+#!/bin/bash
+apt update
+apt install curl git zsh screen
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-source ~/.zshrc
+source ~/.bashrc
 nvm install 14
 npm install --global yarn
-npm install -g typescript
+npm install --global typescript
+npm install
