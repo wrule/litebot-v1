5014df447d851edc90fb1c5a2a766371e4aeadef
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-14 15:11:33 +0800

    watcher

--- index.ts
+++ index.ts
@@ -1,34 +1,23 @@
-import { IParams, SRSI_Martin } from './robot/spot/srsi_martin';
-const ETH_USDT_1h = require('./data/ETH_USDT-30m.json');
-import { ArrayToKLine } from './common/kline';
-import { TestSpot } from './executor/spot/test_spot';
-import { Optimizer } from './optimizer';
-import { Logger } from './utils/logger';
+import { binance } from 'ccxt';
+import { KLineWatcher } from './watcher/kline_watcher';
 
-const kline = ArrayToKLine(ETH_USDT_1h as any[]);
-
-console.log('30m', kline.length);
-
-async function back_testing(params: IParams) {
-  const executor = new TestSpot({ symbol: 'ETH/USDT', fee: 0.001, init_funds_amount: 100 });
-  const robot = new SRSI_Martin({ name: 'test', params, executor });
-  await robot.BackTesting(kline);
-  return { output: executor.Valuation(1600) }
-}
+const secret = require('../.secret.json');
 
 async function main() {
-  const opt = new Optimizer({
-    space: [
-      { name: 'rsi_size', range: [13, 13], },
-      { name: 'k_size', range: [30, 34], },
-      { name: 'd_size', range: [43, 47], },
-      { name: 'stoch_size', range: [43, 47], },
-    ],
-    objective_function: back_testing,
-    loss_function: (output) => 1 / output.output,
-    logger: new Logger(),
+  console.log('你好，世界');
+  const client = new binance({
+    apiKey: secret.API_KEY,
+    secret: secret.SECRET_KEY,
+    enableRateLimit: true,
   });
-  opt.Search();
+  console.log('加载市场');
+  await client.loadMarkets();
+  console.log('加载完成');
+  const watcher = new KLineWatcher(client, 1000, 'ETH/USDT', '30m', 100);
+  watcher.Subscribe((kline) => {
+    console.log(kline.length);
+  });
+  watcher.Start();
 }
 
 main();
