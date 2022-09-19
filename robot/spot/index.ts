92613e954ecd272b84781ef854fdbe85a7b85f2d
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-19 14:30:11 +0800

    代码整理

--- robot/spot/index.ts
+++ robot/spot/index.ts
@@ -162,7 +162,6 @@ abstract class SpotRobot<
       const active_signal = { time: active_candle.time, close: active_candle.close } as ITimeClose;
       // 历史信号
       let last_historical_signal = last_historical_candle as SignalData | null;
-
       // 准备交易
       let tn: ITransaction | null = null;
       // 备份历史时间
@@ -180,7 +179,6 @@ abstract class SpotRobot<
         setImmediate(() => this.logger.log('活跃信号:', active_signal));
         tn = (await this.stop_loss(active_signal)) || null;
       }
-
       // 尝试填充赌局信息
       this.fill_game_id(tn);
       // 是否产生了新的历史数据
