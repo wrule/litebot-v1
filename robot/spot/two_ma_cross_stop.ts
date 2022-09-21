7604f4a262c4baec01b79e74aeb8d7e6434546db
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-21 21:47:58 +0800

    提交代码

--- robot/spot/two_ma_cross_stop.ts
+++ robot/spot/two_ma_cross_stop.ts
@@ -83,7 +83,7 @@ extends SpotRobot<IParams, IOHLCV, ISignal, ISnapshot> {
       const rate = signal.close / this.buy_tn.price;
       if (rate < 0.9) {
         const sell_tn = await this.config.executor.SellAll(
-          signal.close,
+          lagging ? signal.close : signal.close,
           signal.time,
         );
         this.buy_tn = null;
