492b8cb3d602701b3921034878a9cda1cb3a5ca9
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-24 22:53:17 +0800

    feat: 提交代码

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import { KLineWatcher } from './watcher/kline_watcher';
 import { ArrayToKLine } from './common/kline';
 import { SRSI_Martin } from './robot/spot/srsi_martin';
 
-const HistData = require('../data/ETH_USDT-1m.json');
+const HistData = require('../data/BTC_USDT-1h.json');
 const secret = require('../.secret.json');
 
 async function main() {
@@ -29,13 +29,13 @@ async function main() {
       k_size: 32,
       d_size: 45,
       stoch_size: 45,
-      stop_rate: 0.01,
+      stop_rate: 0.99,
     },
     executor,
   });
   const kline = ArrayToKLine(HistData);
   await robot.BackTesting(kline);
-  const valuation = await executor.Valuation(1690.48);
+  const valuation = await executor.Valuation(19347.9701);
   console.log(valuation);
   // const watcher = new KLineWatcher(client, 1000, 'ETH/USDT', '1m', robot.WatchLength);
   // watcher.Subscribe((kline) => {
