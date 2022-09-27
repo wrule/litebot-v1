5d8c71b2b6b69e8cfb68765f87a621cff041189c
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-27 14:39:10 +0800

    注释掉错误

--- example/optimization.ts
+++ example/optimization.ts
@@ -21,21 +21,21 @@ async function back_testing(params: IParams): Promise<IFunctionOutput<any>> {
 async function main() {
   console.log(await back_testing({ fast_size: 10, slow_size: 41, stop_rate: 0.0311 }));
   return;
-  const opt = new Optimizer({
-    space: [
-      { name: 'fast_size', range: [2, 20], },
-      { name: 'slow_size', range: [60, 80], },
-    ],
-    objective_function: back_testing,
-    loss_function: (output) => 1 / output.output,
-    input_filter: (input) => Math.abs(input.slow_size - input.fast_size) > 29,
-    input_mapper: (input) => ({
-      fast_size: input.fast_size < input.slow_size ? input.fast_size : input.slow_size,
-      slow_size: input.fast_size < input.slow_size ? input.slow_size : input.fast_size,
-    }),
-    logger: new Logger(),
-  });
-  opt.Search();
+  // const opt = new Optimizer({
+  //   space: [
+  //     { name: 'fast_size', range: [2, 20], },
+  //     { name: 'slow_size', range: [60, 80], },
+  //   ],
+  //   objective_function: back_testing,
+  //   loss_function: (output) => 1 / output.output,
+  //   input_filter: (input) => Math.abs(input.slow_size - input.fast_size) > 29,
+  //   input_mapper: (input) => ({
+  //     fast_size: input.fast_size < input.slow_size ? input.fast_size : input.slow_size,
+  //     slow_size: input.fast_size < input.slow_size ? input.slow_size : input.fast_size,
+  //   }),
+  //   logger: new Logger(),
+  // });
+  // opt.Search();
 }
 
 main();
