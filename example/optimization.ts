81e19eb70c1b206ef118e875d420bb5f3a0d1f55
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-27 12:22:33 +0800

    eth均线

--- example/optimization.ts
+++ example/optimization.ts
@@ -5,21 +5,21 @@ import { IFunctionOutput, Optimizer } from '../optimizer';
 import { IParams, TwoMaCross } from '../robot/spot/two_ma_cross_stop';
 import { Logger } from '../utils/logger';
 
-const ohlcv_data = require('../../data/BTC_USDT-1h.json');
+const ohlcv_data = require('../../data/ETH_USDT-2h.json');
 const kline = ArrayToKLine(ohlcv_data);
 
 let count = 0;
 
 async function back_testing(params: IParams): Promise<IFunctionOutput<any>> {
-  const executor = new TestSpot({ symbol: 'BTC/USDT', fee: 0.001, init_funds_amount: 100 });
+  const executor = new TestSpot({ symbol: 'ETH/USDT', fee: 0.001, init_funds_amount: 100 });
   const robot = new TwoMaCross({ params, executor });
   await robot.BackTesting(kline);
   console.log(count++);
-  return { output: executor.Valuation(21656.87) };
+  return { output: executor.Valuation(1690.48) };
 }
 
 async function main() {
-  console.log(await back_testing({ fast_size: 12, slow_size: 71, stop_rate: 0.05 }));
+  console.log(await back_testing({ fast_size: 9, slow_size: 44, stop_rate: 0.03 }));
   return;
   const opt = new Optimizer({
     space: [
