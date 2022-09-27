31a3430d0b48522b90397f1253a8d4c6edc07de7
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-27 11:50:14 +0800

    提交代码

--- example/optimization.ts
+++ example/optimization.ts
@@ -2,7 +2,7 @@ import { MemoryReport } from '../report/memory_report';
 import { ArrayToKLine } from '../common/kline';
 import { TestSpot } from '../executor/spot/test_spot';
 import { IFunctionOutput, Optimizer } from '../optimizer';
-import { IParams, TwoMaCross } from '../robot/spot/two_ma_cross';
+import { IParams, TwoMaCross } from '../robot/spot/two_ma_cross_stop';
 import { Logger } from '../utils/logger';
 
 const ohlcv_data = require('../../data/BTC_USDT-1h.json');
@@ -19,16 +19,16 @@ async function back_testing(params: IParams): Promise<IFunctionOutput<any>> {
 }
 
 async function main() {
-  // console.log(await back_testing({ fast_size: 9, slow_size: 44, }));
-  // return;
+  console.log(await back_testing({ fast_size: 12, slow_size: 71, stop_rate: 0.05 }));
+  return;
   const opt = new Optimizer({
     space: [
-      { name: 'fast_size', range: [2, 100], },
-      { name: 'slow_size', range: [2, 100], },
+      { name: 'fast_size', range: [2, 20], },
+      { name: 'slow_size', range: [60, 80], },
     ],
     objective_function: back_testing,
     loss_function: (output) => 1 / output.output,
-    input_filter: (input) => Math.abs(input.slow_size - input.fast_size) > 9,
+    input_filter: (input) => Math.abs(input.slow_size - input.fast_size) > 29,
     input_mapper: (input) => ({
       fast_size: input.fast_size < input.slow_size ? input.fast_size : input.slow_size,
       slow_size: input.fast_size < input.slow_size ? input.slow_size : input.fast_size,
