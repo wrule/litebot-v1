d13a4af949d3724ebb8fcd8df21f732fda7641de
jimao <jimao@jimaodeMBP.lan>
2022-09-28 22:33:28 +0800

    取消活跃信号

--- robot/spot/srsi_martin/index.ts
+++ robot/spot/srsi_martin/index.ts
@@ -122,27 +122,27 @@ extends SpotRobot<IParams, IOHLCV, ISignal, ISnapshot> {
     }
   }
 
-  protected override async stop_signal_action(signal: ITimeClose, lagging?: boolean) {
-    if (this.buy_tn) {
-      const queue_low = this.queue_low();
-      if (signal.close < queue_low) {
-        const sell_tn = await this.config.executor.SellAll(
-          lagging ? queue_low : signal.close,
-          signal.time,
-        );
-        this.buy_tn = null;
-        return sell_tn;
-      }
+  // protected override async stop_signal_action(signal: ITimeClose, lagging?: boolean) {
+  //   if (this.buy_tn) {
+  //     const queue_low = this.queue_low();
+  //     if (signal.close < queue_low) {
+  //       const sell_tn = await this.config.executor.SellAll(
+  //         lagging ? queue_low : signal.close,
+  //         signal.time,
+  //       );
+  //       this.buy_tn = null;
+  //       return sell_tn;
+  //     }
 
-      const target_price = this.buy_tn.price * (1 - this.config.params.stop_rate);
-      if (signal.close < target_price) {
-        const sell_tn = await this.config.executor.SellAll(
-          lagging ? target_price : signal.close,
-          signal.time,
-        );
-        this.buy_tn = null;
-        return sell_tn;
-      }
-    }
-  }
+  //     const target_price = this.buy_tn.price * (1 - this.config.params.stop_rate);
+  //     if (signal.close < target_price) {
+  //       const sell_tn = await this.config.executor.SellAll(
+  //         lagging ? target_price : signal.close,
+  //         signal.time,
+  //       );
+  //       this.buy_tn = null;
+  //       return sell_tn;
+  //     }
+  //   }
+  // }
 }
