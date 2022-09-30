b9eb731c1d11b96772f155cfd889dea8d7650caf
jimao <jimao@jimaodeMacBook-Pro.local>
2022-09-30 13:03:20 +0800

    提交

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 echo 🚀 此脚本将帮助你:
 echo 1. 部署node,curl,screen,zsh等环境或工具
 echo 2. 安装项目所需要的npm依赖
-echo 3. 编译构建项目
+echo 3. 编译项目
 echo 😄 如果遇到询问确认，请选择[y/yes]
 sleep 3
 apt update
@@ -16,5 +16,6 @@ npm install --global yarn
 npm install --global typescript
 npm install
 npm run build
+echo ✨ 项目编译成功
 echo 🚀 安装ohmyzsh中，完成后建议重新启动终端...
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
