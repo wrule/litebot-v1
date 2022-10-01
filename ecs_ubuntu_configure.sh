b138718c570fc0511961ec44ad78de8809f0e56a
jimao <jimao@jimaodeMBP.lan>
2022-10-01 21:05:06 +0800

    提交代码

--- ecs_ubuntu_configure.sh
+++ ecs_ubuntu_configure.sh
@@ -4,13 +4,13 @@
 BRANCH=220829-core
 echo 🚀 此脚本将帮助你:
 echo 1. 下载项目源代码
-echo 2. 安装node,git,curl,screen,zsh等环境或工具
+echo 2. 安装nodejs环境
 echo 3. 安装项目所需要的npm依赖
 echo 4. 编译项目
 echo 😄 如果遇到询问确认，请选择[y/yes]
 sleep 5
-apt update
-apt install git curl screen zsh
+# apt update
+# apt install git curl screen zsh
 [ ! -e package.json ] \
 && git clone https://github.com/wrule/litebot.git \
 && cd litebot \
@@ -26,5 +26,5 @@ npm install --global typescript
 npm install
 npm run build
 echo ✨ 项目编译成功
-echo 🚀 安装ohmyzsh中，完成后建议重新启动终端...
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# echo 🚀 安装ohmyzsh中，完成后建议重新启动终端...
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
