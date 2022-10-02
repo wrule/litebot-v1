3f8d53f068dc296a13ccb7308cebefef53d9221b
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-02 13:55:45 +0800

    一键部署命令

--- shell/debian_init.sh
+++ shell/debian_init.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# 一键部署命令
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/wrule/litebot/220829-core/shell/debian_init.sh)"
 apt update
 apt install git curl screen zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/wrule/litebot/220829-core/shell/configure.sh)"
--- shell/redhat_init.sh
+++ shell/redhat_init.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# 一键部署命令
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/wrule/litebot/220829-core/shell/redhat_init.sh)"
 yum update
 yum install git curl screen zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/wrule/litebot/220829-core/shell/configure.sh)"
