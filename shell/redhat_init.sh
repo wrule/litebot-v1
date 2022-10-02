d57ccd15368561f0d07a75e3f463514191002c9f
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-02 22:42:06 +0800

    提交代码

--- shell/redhat_init.sh
+++ shell/redhat_init.sh
@@ -4,4 +4,5 @@
 yum update
 yum install git curl screen zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/wrule/litebot/220829-core/shell/configure.sh)"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/wrule/litebot/220829-core/shell/zsh_install.sh)"
+echo 🚀 安装ohmyzsh中，完成后建议重新启动终端...
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- shell/zsh_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# 一键部署命令
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/wrule/litebot/220829-core/shell/zsh_install.sh)"
-echo 🚀 安装ohmyzsh中，完成后建议重新启动终端...
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
