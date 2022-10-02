7ed7de9e31a13ad53f855865861252f4131b19aa
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-02 13:44:34 +0800

    提交

--- redhat_init.sh
+++ redhat_init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 yum update
 yum install git curl screen zsh
+./configure.sh
 echo 🚀 安装ohmyzsh中，完成后建议重新启动终端...
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
