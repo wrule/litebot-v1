2030d76bfb30a2587bb0eed79d63786f43bc1923
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-02 13:34:35 +0800

    提交代码

--- configure.sh
+++ configure.sh
@@ -3,23 +3,20 @@
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/wrule/litebot/220829-core/configure.sh)"
 BRANCH=220829-core
 echo 🚀 此脚本将帮助你:
-echo 1. 下载项目源代码
-echo 2. 安装nodejs环境
+echo 1. 安装nodejs环境
+echo 2. 下载项目源代码
 echo 3. 安装项目所需要的npm依赖
 echo 4. 编译项目
 echo 😄 如果遇到询问确认，请选择[y/yes]
 sleep 5
-[ ! -e package.json ] \
-&& git clone https://github.com/wrule/litebot.git \
-&& cd litebot \
-&& git checkout $BRANCH \
-&& git pull
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install 14
 npm install --global typescript
+[ ! -e package.json ] && git clone https://github.com/wrule/litebot.git && cd litebot
+git checkout $BRANCH && git pull
 npm install
 npm run build
 echo ✨ 项目编译成功
