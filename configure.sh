40763d7743a7412ecb9455b7ccb9020c141525a6
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-02 13:39:53 +0800

    安装yarn

--- configure.sh
+++ configure.sh
@@ -14,6 +14,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install 14
+npm install --global yarn
 npm install --global typescript
 [ ! -e package.json ] && git clone https://github.com/wrule/litebot.git && cd litebot
 git checkout $BRANCH && git pull
