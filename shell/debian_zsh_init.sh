901ccc425371881bb6909f38c50aabf3e080f705
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-02 22:44:36 +0800

    提交代码

--- shell/debian_zsh_init.sh
+++ shell/debian_zsh_init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # 一键部署命令
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/wrule/litebot/220829-core/shell/debian_init.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/wrule/litebot/220829-core/shell/debian_zsh_init.sh)"
 apt update
 apt install git curl screen zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/wrule/litebot/220829-core/shell/configure.sh)"
-echo 🚀 安装ohmyzsh中，完成后建议重新启动终端...
+echo 🚀 安装ohmyzsh中...
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- shell/redhat_zsh_init.sh
+++ shell/redhat_zsh_init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # 一键部署命令
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/wrule/litebot/220829-core/shell/redhat_init.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/wrule/litebot/220829-core/shell/redhat_zsh_init.sh)"
 yum update
 yum install git curl screen zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/wrule/litebot/220829-core/shell/configure.sh)"
-echo 🚀 安装ohmyzsh中，完成后建议重新启动终端...
+echo 🚀 安装ohmyzsh中...
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
