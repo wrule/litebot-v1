60d67457412d08e798fdd452a41ad75954f3d8e2
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-01 12:43:00 +0800

    整理代码

--- robot/spot/super_trend.ts
+++ robot/spot/super_trend.ts
@@ -72,6 +72,8 @@ extends SpotRobot<IParams, IOHLCV, ISignal, ISnapshot> {
   }
 
   protected generate_signal_data(historical_data: IOHLCV[]): ISignal[] {
+    const close_r1: (number | null)[] = historical_data.map((history) => history.close);
+    close_r1.pop() && close_r1.unshift(null);
     const hl2 = historical_data.map((history) => (history.high + history.low) / 2);
     const atr = this.atr(
       historical_data.map((history) => history.high),
@@ -79,25 +81,19 @@ extends SpotRobot<IParams, IOHLCV, ISignal, ISnapshot> {
       historical_data.map((history) => history.close),
       this.config.params,
     );
-    const up = hl2.map(
-      (item, index) =>
-        atr[index] != null ? item - atr[index] * this.config.params.atr_multiplier : null
-    );
-    const up_r1 = up.slice();
-    up_r1.pop() && up_r1.unshift(null);
-    const close_r1: (number | null)[] = historical_data.map((history) => history.close);
-    close_r1.pop() && close_r1.unshift(null);
 
-    let up_max = -Infinity;
-    const up_border = up_r1.map((item, index) => {
+    const atr_down_r1 = hl2.map((item, index) => atr[index] != null ? item - atr[index] * this.config.params.atr_multiplier : null);
+    atr_down_r1.pop() && atr_down_r1.unshift(null);
+    let atr_down_max = -Infinity;
+    const down_border = atr_down_r1.map((item, index) => {
       const close = close_r1[index];
       if (item == null || close == null) return null;
-      let new_item = up_max;
-      if (item > up_max) up_max = new_item = item;
-      if (close < new_item) up_max = -Infinity;
+      let new_item = atr_down_max;
+      if (item > atr_down_max) atr_down_max = new_item = item;
+      if (close < new_item) atr_down_max = -Infinity;
       return new_item;
     });
-    console.log(up_border.slice(up_border.length - 40));
+    console.log(down_border.slice(down_border.length - 40));
     return [];
     // const close = historical_data.map((history) => history.close);
     // const { fast_line, slow_line, diff } = this.double_sma(close, this.config.params);
