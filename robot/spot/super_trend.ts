5efe43a2dfe2ca6304aa110d54bad8d42e6d15fd
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-01 14:37:04 +0800

    super_trend_channel

--- robot/spot/super_trend.ts
+++ robot/spot/super_trend.ts
@@ -72,6 +72,43 @@ extends SpotRobot<IParams, IOHLCV, ISignal, ISnapshot> {
     // return this.double_sma_start(this.config.params) + 2;
   }
 
+  private super_trend_channel(
+    source: number[],
+    high: number[],
+    low: number[],
+    close: number[],
+  ) {
+    const atr = this.atr(high, low, close, this.config.params);
+    const close_r1: (number | null)[] = close.slice();
+    close_r1.pop() && close_r1.unshift(null);
+
+    const atr_down_r1 = source.map((item, index) => atr[index] != null ? item - atr[index] * this.config.params.atr_multiplier : null);
+    atr_down_r1.pop() && atr_down_r1.unshift(null);
+    let atr_down_max = -Infinity;
+    const down_border = atr_down_r1.map((item, index) => {
+      const close = close_r1[index];
+      if (item == null || close == null) return null;
+      let new_item = atr_down_max;
+      if (item > atr_down_max) atr_down_max = new_item = item;
+      if (close < new_item) atr_down_max = -Infinity;
+      return new_item;
+    });
+
+    const atr_up_r1 = source.map((item, index) => atr[index] != null ? item + atr[index] * this.config.params.atr_multiplier : null);
+    atr_up_r1.pop() && atr_up_r1.unshift(null);
+    let atr_up_min = Infinity;
+    const up_border = atr_up_r1.map((item, index) => {
+      const close = close_r1[index];
+      if (item == null || close == null) return null;
+      let new_item = atr_up_min;
+      if (item < atr_up_min) atr_up_min = new_item = item;
+      if (close > new_item) atr_up_min = Infinity;
+      return new_item;
+    });
+
+    return { up: up_border, down: down_border };
+  }
+
   protected generate_signal_data(historical_data: IOHLCV[]): ISignal[] {
     const hl2 = historical_data.map((history) => (history.high + history.low) / 2);
     const atr = this.atr(
