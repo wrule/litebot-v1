29a6552fd7b881060189ae852af18b2c7bc1a24e
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-05 01:21:53 +0800

    karma

--- robot/spot/karma.ts
+++ robot/spot/karma.ts
@@ -20,7 +20,7 @@ extends IOHLCV {
 }
 
 export
-class TwoMaCross
+class Karma
 extends SpotRobot<IParams, IOHLCV, ISignal, ISnapshot> {
   public constructor(config: ISpotRobotConfig<IParams, IOHLCV, ISignal, ISnapshot>) {
     super(config);
