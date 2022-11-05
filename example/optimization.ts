1b3f122f1eca1b58abc1a8b464c7d0ee32cbf86e
jimao <jimao@jimaodeMBP.lan>
2022-11-05 15:20:32 +0800

    提交代码

--- example/optimization.ts
+++ example/optimization.ts
@@ -1,15 +1,15 @@
 import { ArrayToKLine } from '../common/kline';
 import { TestSpot } from '../executor/spot/test_spot';
 import { IFunctionOutput, Optimizer } from '../optimizer';
-import { IParams, SRSI_Martin } from '../robot/spot/srsi_martin';
+import { IParams, Karma } from '../robot/spot/karma';
 import { Logger } from '../utils/logger';
 
-const ohlcv_data = require('../../data/SOL_BUSD-2h.json');
+const ohlcv_data = require('../../data/BTC_USDT-2h.json');
 const kline = ArrayToKLine(ohlcv_data);
 
 async function back_testing(params: IParams): Promise<IFunctionOutput<any>> {
   const executor = new TestSpot({ symbol: 'ETH/USDT', fee: 0.001, init_funds_amount: 100 });
-  const robot = new SRSI_Martin({ params, executor });
+  const robot = new Karma({ params, executor });
   await robot.BackTesting(kline);
   return { output: executor.Valuation(kline[kline.length - 1].close) };
 }
@@ -17,12 +17,12 @@ async function back_testing(params: IParams): Promise<IFunctionOutput<any>> {
 async function main() {
   const opt = new Optimizer({
     space: [
-      { name: 'rsi_size', range: [2, 100], },
-      { name: 'k_size', range: [2, 100], },
-      { name: 'd_size', range: [2, 100], },
-      { name: 'stoch_size', range: [2, 100], },
-      // { name: 'fast_size', range: [2, 100], },
-      // { name: 'slow_size', range: [2, 100], },
+      // { name: 'rsi_size', range: [2, 100], },
+      // { name: 'k_size', range: [2, 100], },
+      // { name: 'd_size', range: [2, 100], },
+      // { name: 'stoch_size', range: [2, 100], },
+      { name: 'fast_size', range: [2, 100], },
+      { name: 'slow_size', range: [2, 100], },
       // { name: 'atr_period', range: [2, 30], },
       // { name: 'atr_multiplier', range: [2, 5], },
       // { name: 'fast', range: [2, 100], },
@@ -32,11 +32,11 @@ async function main() {
     objective_function: back_testing,
     loss_function: (output) => 1 / output.output,
     // input_filter: (input) => Math.abs(input.slow - input.fast) > 5,
-    // input_mapper: (input) => ({
-    //   fast: input.fast < input.slow ? input.fast : input.slow,
-    //   slow: input.fast < input.slow ? input.slow : input.fast,
-    //   smooth: input.smooth,
-    // }),
+    input_mapper: (input) => ({
+      fast_size: input.fast_size < input.slow_size ? input.fast_size : input.slow_size,
+      slow_size: input.fast_size < input.slow_size ? input.slow_size : input.fast_size,
+      // smooth: input.smooth,
+    }),
     logger: new Logger(),
   });
   opt.Search();
