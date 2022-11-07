613db04d868ad3f7f1fc08cfe39a781158086a40
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-07 22:57:06 +0800

    制作中

--- robot/spot/kd_sma_diff.ts
+++ robot/spot/kd_sma_diff.ts
@@ -5,7 +5,8 @@ import { ISnapshot } from '@/common/snapshot';
 
 export
 interface IParams {
-  rsi_size: number;
+  fast_size: number;
+  slow_size: number;
   k_size: number;
   d_size: number;
 }
@@ -15,44 +16,64 @@ interface ISignal
 extends IOHLCV {
   buy?: boolean;
   sell?: boolean;
-  rsi?: number;
+  fast?: number;
+  slow?: number;
   k?: number;
   d?: number;
   diff?: number;
 }
 
 export
-class KDRSI
+class KD_SMA_DIFF
 extends SpotRobot<IParams, IOHLCV, ISignal, ISnapshot> {
   public constructor(config: ISpotRobotConfig<IParams, IOHLCV, ISignal, ISnapshot>) {
     super(config);
   }
 
-  private kdrsi(close: number[], options: {
-    rsi_size: number,
-    k_size: number,
-    d_size: number,
+  private kd_sma_diff(close: number[], options: {
+    fast_size: number;
+    slow_size: number;
+    k_size: number;
+    d_size: number;
   }) {
-    let rsi: number[] = [];
-    const rsi_start = tulind.indicators.rsi.start([options.rsi_size]);
-    tulind.indicators.rsi.indicator(
+    let fast_line: number[] = [];
+    const fast_start = tulind.indicators.sma.start([options.fast_size]);
+    tulind.indicators.sma.indicator(
       [close],
-      [options.rsi_size],
+      [options.fast_size],
       (error: any, data: any) => {
         if (error) throw error;
-        rsi = data[0];
+        fast_line = data[0];
       },
     );
+    fast_line = Array(fast_start).fill(null).concat(fast_line);
+
+    let slow_line: number[] = [];
+    const slow_start = tulind.indicators.sma.start([options.slow_size]);
+    tulind.indicators.sma.indicator(
+      [close],
+      [options.slow_size],
+      (error: any, data: any) => {
+        if (error) throw error;
+        slow_line = data[0];
+      },
+    );
+    slow_line = Array(slow_start).fill(null).concat(slow_line);
+
+    let sma_diff = fast_line.map((item, index) => item - slow_line[index]);
+    sma_diff = sma_diff.slice(slow_start);
+
     let k: number[] = [];
     const k_start = tulind.indicators.sma.start([options.k_size]);
     tulind.indicators.sma.indicator(
-      [rsi],
+      [sma_diff],
       [options.k_size],
       (error: any, data: any) => {
         if (error) throw error;
         k = data[0];
       },
     );
+
     let d: number[] = [];
     const d_start = tulind.indicators.sma.start([options.d_size]);
     tulind.indicators.sma.indicator(
@@ -63,6 +84,7 @@ extends SpotRobot<IParams, IOHLCV, ISignal, ISnapshot> {
         d = data[0];
       },
     );
+
     rsi = Array(rsi_start).fill(null).concat(rsi);
     k = Array(rsi_start + k_start).fill(null).concat(k);
     d = Array(rsi_start + k_start + d_start).fill(null).concat(d);
@@ -71,9 +93,10 @@ extends SpotRobot<IParams, IOHLCV, ISignal, ISnapshot> {
   }
 
   private kdrsi_start(options: {
-    rsi_size: number,
-    k_size: number,
-    d_size: number,
+    fast_size: number;
+    slow_size: number;
+    k_size: number;
+    d_size: number;
   }) {
     const rsi_start = tulind.indicators.rsi.start([options.rsi_size]);
     const k_start = tulind.indicators.sma.start([options.k_size]);
