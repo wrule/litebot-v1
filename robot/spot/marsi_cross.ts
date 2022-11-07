a2a3b809cb98f31faa4827cc09a10d18c74221c2
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-07 17:57:00 +0800

    基本完成

--- robot/spot/marsi_cross.ts
+++ robot/spot/marsi_cross.ts
@@ -67,18 +67,27 @@ extends SpotRobot<IParams, IOHLCV, ISignal, ISnapshot> {
     return { rsi, k, d, diff };
   }
 
+  private kdrsi_start(options: {
+    rsi_size: number,
+    k_size: number,
+    d_size: number,
+  }) {
+    const rsi_start = tulind.indicators.rsi.start([options.rsi_size]);
+    const k_start = tulind.indicators.sma.start([options.k_size]);
+    const d_start = tulind.indicators.sma.start([options.d_size]);
+    return rsi_start + k_start + d_start;
+  }
+
   protected ready_length() {
-    return this.double_sma_start(this.config.params) + 2;
+    return this.kdrsi_start(this.config.params) + 2;
   }
 
   protected generate_signal_data(historical_data: IOHLCV[]): ISignal[] {
     const close = historical_data.map((history) => history.close);
-    const { fast_line, slow_line, diff } = this.double_sma(close, this.config.params);
+    const { rsi, k, d, diff } = this.kdrsi(close, this.config.params);
     return this.fill_signal_data(historical_data, (signal, index) => {
       const diff_last = diff[index];
       const diff_prev = diff[index - 1];
-      signal.fast_ma = fast_line[index];
-      signal.slow_ma = slow_line[index];
       signal.diff = diff[index];
       if (diff_last > 0 && diff_prev <= 0) signal.buy = true;
       if (diff_last < 0 && diff_prev >= 0) signal.sell = true;
